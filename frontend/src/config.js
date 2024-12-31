8194f26007b5e684e02841dea69b9035a488a7cb
UckTay <56901754+UckTay@users.noreply.github.com>
2024-12-30 22:50:21 -0500

    updated ports

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // config.js
-export const BASE_URL = `${window.location.protocol}//${window.location.hostname}:5000`; 
+export const BASE_URL = `${window.location.protocol}//${window.location.hostname}:443`;
